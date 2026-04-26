c7e06d63b38ea227bebcce77ea065ccd53173a8f
dianamaliha <241230056@unmuhpnk.ac.id>
2026-04-26 23:11:56 +0700

    menambah isi kontak dan isi profil

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -1,1 +1,6 @@
-<h1>Halaman Kontak</h1>
+<div>
+    <h1>Halaman Kontak</h1>
+    <p><mark> Kontak yang dapat dihubungi: </mark></p>
+    <p>Email: 241230056@unmuhpnk.ac.id</p>
+    <p>Nomor: 088242840768</p>
+</div>
--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,1 +1,7 @@
-<h1>Halaman Profil</h1>
+<div>
+    <h1>Halaman Profil</h1>
+    <p>Nama: Diana Maliha Muntaz</p>
+    <p>Nim: 241230056</p>
+    <p>Kelas:: 06 B</p>
+    <p>Prodi: Sistem Informasi</p>
+</div>
